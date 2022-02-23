392d30554001374184b7c0ac5ba53dfa84349e1e
Estefania Jimenez Garcia <estefaniajimgar@gmail.com>
2022-02-23 14:46:00 -0600

    Finished intro portion

--- src/components/Intro/intro.jsx
+++ src/components/Intro/intro.jsx
@@ -1,7 +1,36 @@
+import "./intro.css"
+import Me from "../../img/me.jpeg"
+
 const Intro = () => {
   return (
-    <div>
-    this is intro
+    <div className="i">
+
+    <div className="i-left">
+      <div className="i-left-wrapper">
+        <h2 className="i-intro"> Hello, My name is </h2>
+        <h1 className="i-name"> Estefania </h1>
+
+        <div className="i-title">
+
+          <div className="i-title-wrapper">
+            <div className="i-title-item">Software Engineer Student</div>
+            <div className="i-title-item">Software Engineer</div>
+          </div>
+
+          <div className="i-desc">
+            I am currently a student at Tec de Monterrey
+          </div>
+
+        </div>
+
+      </div>
+    </div>
+
+    <div className="i-right">
+      <div className="i-bg"> </div>
+      <img src={Me} alt="" className="i-img" />
+
+    </div>
     </div>
   )
 }
