699f700541138b610d84d8cf4f3755220f37dec1
Estefania Jimenez Garcia <estefaniajimgar@gmail.com>
2022-02-23 14:01:39 -0600

    added intro

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import Intro from "./components/intro/Intro";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1> React Portafolio </h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  return <div><Intro/></div>;
+};
 
 export default App;
