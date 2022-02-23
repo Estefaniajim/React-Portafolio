3253e5ec564d4d68bba15567a384a43ac8929de6
Estefania Jimenez Garcia <estefaniajimgar@gmail.com>
2022-02-23 13:12:49 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1> React app for my portafolio </h1>
+        <h1> React Portafolio </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
