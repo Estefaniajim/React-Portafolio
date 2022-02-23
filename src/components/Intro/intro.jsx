2f670b2cf23acd6ddae4a138b378dbf7a92583e2
Estefania Jimenez Garcia <estefaniajimgar@gmail.com>
2022-02-23 14:58:10 -0600

    changes color and img

--- src/components/Intro/intro.jsx
+++ src/components/Intro/intro.jsx
@@ -26,7 +26,7 @@ const Intro = () => {
 
     <div className="i-right">
       <div className="i-bg"> </div>
-      <img src={Me} alt="" className="i-img" />
+      <img src={Me} alt="" className="i-img"/>
 
     </div>
     </div>
